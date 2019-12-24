cad465494d6d4d4ab9fe7a16b99e033035abd6af
adadevx <53271823+adadevx@users.noreply.github.com>
2019-12-24 09:24:14 +0700

    Update SplashActivity.kt

--- app/src/main/java/net/adadev/browser5g/SplashActivity.kt
+++ app/src/main/java/net/adadev/browser5g/SplashActivity.kt
@@ -38,7 +38,7 @@ class SplashActivity : AppCompatActivity() {
         }
 
         mInterstitialAd = InterstitialAd(this)
-        mInterstitialAd.adUnitId = "ca-app-pub-3940256099942544/1033173712"
+        mInterstitialAd.adUnitId = ""
         mInterstitialAd.loadAd(AdRequest.Builder().build())
 
         mInterstitialAd.adListener = object : AdListener() {
